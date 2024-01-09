e89c332da17f6f72fab61f8962e250a59362b20b
Carlos Paredes <00028516@uca.edu.sv>
2024-01-09 12:15:49 -0600

    NearMe project Mohamed Course

---
+++ NearMe/NearMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NearMe
+//
+//  Created by Carlos Paredes on 9/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
