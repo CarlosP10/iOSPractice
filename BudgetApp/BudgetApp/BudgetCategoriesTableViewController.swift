a0ba8a44051efd3f197b7cb532fb3f872dcaab73
Carlos Paredes <00028516@uca.edu.sv>
2024-01-10 13:43:34 -0600

    [ADD] - BudgetApp Creation

---
+++ BudgetApp/BudgetApp/BudgetCategoriesTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BudgetCategoriesTableViewController.swift
+//  BudgetApp
+//
+//  Created by Carlos Paredes on 10/1/24.
+//
+
+import UIKit
+
+class BudgetCategoriesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
