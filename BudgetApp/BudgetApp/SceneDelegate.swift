eee2caad070a6fc0ca4e785acf20c3f4fc44a995
Carlos Paredes <00028516@uca.edu.sv>
2024-01-10 13:52:44 -0600

    [ADD] - Setting up Core Data Stack

--- BudgetApp/BudgetApp/SceneDelegate.swift
+++ BudgetApp/BudgetApp/SceneDelegate.swift
@@ -6,19 +6,28 @@
 //
 
 import UIKit
+import CoreData
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "BudgetModel")
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                fatalError("Unable to load persistent stores: \(error)")
+            }
+        }
+        
+        return container
+    }()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = BudgetCategoriesTableViewController()
+        window.rootViewController = BudgetCategoriesTableViewController(persistentContainer: persistentContainer)
         window.makeKeyAndVisible()
         self.window = window
     }
