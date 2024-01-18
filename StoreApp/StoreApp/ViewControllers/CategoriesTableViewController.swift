cf0fc201c7d7236315e36c159dc4c912249bcc2f
Carlos Paredes <00028516@uca.edu.sv>
2024-01-18 12:10:48 -0600

    [UPDATE] - Setting up programatic ui

--- StoreApp/StoreApp/ViewControllers/CategoriesTableViewController.swift
+++ StoreApp/StoreApp/ViewControllers/CategoriesTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CategoriesTableViewController.swift
 //  StoreApp
 //
 //  Created by Carlos Paredes on 18/1/24.
@@ -7,10 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CategoriesTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .orange
         // Do any additional setup after loading the view.
     }
 
