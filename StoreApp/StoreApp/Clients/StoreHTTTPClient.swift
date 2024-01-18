66474540d8466a7608af935082e65a30a777e6d1
Carlos Paredes <00028516@uca.edu.sv>
2024-01-18 12:28:59 -0600

    [UPDATE] - Fetching all categories

---
+++ StoreApp/StoreApp/Clients/StoreHTTTPClient.swift
@@ -0,0 +1,32 @@
+//
+//  StoreHTTTPClient.swift
+//  StoreApp
+//
+//  Created by Carlos Paredes on 18/1/24.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case invalidURL, invalidServerRepsonse, decodingError
+}
+
+class StoreHTTTPClient {
+    
+    func getAllCategories() async throws -> [Category] {
+        
+        let (data, response) = try await URLSession.shared.data(from: URL.allCategories)
+        
+        guard let httpResponse = response as? HTTPURLResponse,
+              httpResponse.statusCode == 200
+        else {
+            throw NetworkError.invalidServerRepsonse
+        }
+        
+        guard let categories = try? JSONDecoder().decode([Category].self, from: data) else {
+            throw NetworkError.decodingError
+        }
+        
+        return categories
+    }
+}
---
+++ StoreApp/StoreApp/Models/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  StoreApp
+//
+//  Created by Carlos Paredes on 18/1/24.
+//
+
+import Foundation
+
+struct Category: Codable {
+    let id: Int
+    let name: String
+    let image: String
+}
