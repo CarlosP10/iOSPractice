90dba651192a83c2336f53eb276209c2be659ff6
Carlos Paredes <00028516@uca.edu.sv>
2024-01-18 12:03:19 -0600

    [ADD] - New project Store App

---
+++ StoreApp/StoreApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreApp
+//
+//  Created by Carlos Paredes on 18/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
