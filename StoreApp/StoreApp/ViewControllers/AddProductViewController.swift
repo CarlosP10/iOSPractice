51cafd5b0c206d0ee2a40b2bcac7578874bf250f
Carlos Paredes <00028516@uca.edu.sv>
2024-01-22 20:39:02 -0600

    [UPDATE] - AddProductViewController

--- StoreApp/StoreApp/ViewControllers/AddProductViewController.swift
+++ StoreApp/StoreApp/ViewControllers/AddProductViewController.swift
@@ -10,10 +10,67 @@ import UIKit
 import SwiftUI
 
 class AddProductViewController: UIViewController {
+    
+    lazy var titleTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter title"
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        textField.leftViewMode = .always
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    lazy var priceTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter price (Numbers only)"
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        textField.leftViewMode = .always
+        textField.borderStyle = .roundedRect
+        textField.keyboardType = .numberPad
+        return textField
+    }()
+    
+    lazy var imageTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter image url"
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        textField.leftViewMode = .always
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    lazy var descriptionTextView: UITextView = {
+        let textView = UITextView()
+        textView.contentInsetAdjustmentBehavior = .automatic
+        textView.backgroundColor = .lightGray
+        return textView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        setupUI()
+    }
+    
+    private func setupUI() {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = UIStackView.spacingUseSystem
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
+        
+        stackView.addArrangedSubview(titleTextField)
+        stackView.addArrangedSubview(priceTextField)
+        stackView.addArrangedSubview(descriptionTextView)
+        stackView.addArrangedSubview(imageTextField)
+        
+        view.addSubview(stackView)
+        
+        //add constraints
+        stackView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        descriptionTextView.heightAnchor.constraint(equalToConstant: 200).isActive = true
     }
     
 }
